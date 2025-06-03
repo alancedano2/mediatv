54db6e95359df1b7bce49cfb320dc1d76a80e2ff
alancedano2 <supernatura94@yahoo.com>
2025-06-03 19:34:22 -0400

    Update epg.xml.js

--- api/epg.xml.js
+++ api/epg.xml.js
@@ -1,119 +1,124 @@
-// api/epg.xml.js
+export async function GET() {
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<tv>
+  <!-- KQ105TV -->
+  <channel id="kq105tv">
+    <display-name>KQ105TV</display-name>
+    <icon src="https://upload.wikimedia.org/wikipedia/commons/3/3f/KQ_105_Logo.png" />
+  </channel>
+  <programme start="20250603000000 -0400" stop="20250604000000 -0400" channel="kq105tv">
+    <title>KQ105 en Vivo</title>
+    <desc>Escucha lo mejor de KQ105 las 24 horas.</desc>
+  </programme>
 
-export const config = {
-  runtime: 'edge',
-};
+  <!-- NetflixEventos -->
+  <channel id="netflixeventos">
+    <display-name>NetflixEventos</display-name>
+    <icon src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" />
+  </channel>
+  <programme start="20250603000000 -0400" stop="20250604000000 -0400" channel="netflixeventos">
+    <title>Eventos Exclusivos Netflix</title>
+    <desc>Disfruta de eventos especiales y estrenos exclusivos.</desc>
+  </programme>
 
-function formatDate(date) {
-  const pad = (n) => n.toString().padStart(2, '0');
-  return (
-    date.getUTCFullYear().toString() +
-    pad(date.getUTCMonth() + 1) +
-    pad(date.getUTCDate()) +
-    pad(date.getUTCHours()) +
-    pad(date.getUTCMinutes()) +
-    pad(date.getUTCSeconds()) +
-    ' +0000'
-  );
-}
+  <!-- PPV 01 -->
+  <channel id="ppv01">
+    <display-name>PPV 01</display-name>
+    <icon src="https://upload.wikimedia.org/wikipedia/commons/c/ca/Shaw_PPV.png" />
+  </channel>
+  <programme start="20250603000000 -0400" stop="20250604000000 -0400" channel="ppv01">
+    <title>PPV 24/7</title>
+    <desc>Visite mediaiptv.vercel.app para más detalles</desc>
+  </programme>
 
-function getProgramsForChannel(channel) {
-  const now = new Date();
-  const day = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), 0, 0, 0));
-  const programs = [];
+  <!-- NBA -->
+  <channel id="nba">
+    <display-name>NBA</display-name>
+    <icon src="https://cdn.freebiesupply.com/images/large/2x/nba-logo-transparent.png" />
+  </channel>
+  <programme start="20250603000000 -0400" stop="20250604000000 -0400" channel="nba">
+    <title>Fuera del Aire</title>
+    <desc>Fuera del Aire</desc>
+  </programme>
+  <programme start="20250605200000 -0400" stop="20250605230000 -0400" channel="nba">
+    <title>NBA Finals Game 1</title>
+    <desc>Indiana Pacers vs Oklahoma City Thunder</desc>
+  </programme>
+  <programme start="20250608200000 -0400" stop="20250608230000 -0400" channel="nba">
+    <title>NBA Finals Game 2</title>
+    <desc>Indiana Pacers vs Oklahoma City Thunder</desc>
+  </programme>
+  <programme start="20250611T200000 -0400" stop="20250611T230000 -0400" channel="nba">
+    <title>NBA Finals Game 3</title>
+    <desc>Indiana Pacers vs Oklahoma City Thunder</desc>
+  </programme>
+  <programme start="20250613200000 -0400" stop="20250613230000 -0400" channel="nba">
+    <title>NBA Finals Game 4</title>
+    <desc>Indiana Pacers vs Oklahoma City Thunder</desc>
+  </programme>
 
-  const addShow = (title, start, end) => {
-    programs.push({ channel, title, start, end });
-  };
+  <!-- MLB -->
+  <channel id="mlb">
+    <display-name>MLB</display-name>
+    <icon src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Major_League_Baseball_logo.svg/1200px-Major_League_Baseball_logo.svg.png" />
+  </channel>
+  <programme start="20250603000000 -0400" stop="20250604000000 -0400" channel="mlb">
+    <title>MLB 24/7</title>
+    <desc>Visite mediaiptv.vercel.app para más detalles</desc>
+  </programme>
 
-  const addDailyShow = (title, hourStart, hourEnd, dayOffset = 0) => {
-    const start = new Date(day.getTime() + dayOffset * 86400000 + hourStart * 3600000);
-    const end = new Date(day.getTime() + dayOffset * 86400000 + hourEnd * 3600000);
-    addShow(title, start, end);
-  };
+  <!-- EWO TV -->
+  <channel id="ewotv">
+    <display-name>EWO TV</display-name>
+    <icon src="https://ewopr-puce.vercel.app/logo.png" />
+  </channel>
+  <programme start="20250603000000 -0400" stop="20250603080000 -0400" channel="ewotv">
+    <title>Fuera del Aire</title>
+    <desc>No hay programación.</desc>
+  </programme>
+  <programme start="20250603080000 -0400" stop="20250603093000 -0400" channel="ewotv">
+    <title>eWo La Verdad Absoluta</title>
+    <desc>Programa informativo</desc>
+  </programme>
+  <programme start="20250603093000 -0400" stop="20250603100000 -0400" channel="ewotv">
+    <title>Cuenta Regresiva para eWo El Update</title>
+    <desc>Previo al programa principal</desc>
+  </programme>
+  <programme start="20250603100000 -0400" stop="20250603110000 -0400" channel="ewotv">
+    <title>eWo El Update</title>
+    <desc>Noticias de eWo</desc>
+  </programme>
+  <programme start="20250603110000 -0400" stop="20250603150000 -0400" channel="ewotv">
+    <title>eWo ThrowBack</title>
+    <desc>Repeticiones clásicas</desc>
+  </programme>
+  <programme start="20250603150000 -0400" stop="20250603153000 -0400" channel="ewotv">
+    <title>eWo El Perfil</title>
+    <desc>Entrevistas</desc>
+  </programme>
+  <programme start="20250603153000 -0400" stop="20250603193000 -0400" channel="ewotv">
+    <title>eWo 24/7 Retro</title>
+    <desc>Programación nostálgica</desc>
+  </programme>
+  <programme start="20250603193000 -0400" stop="20250604000000 -0400" channel="ewotv">
+    <title>Fuera del Aire</title>
+    <desc>No hay programación.</desc>
+  </programme>
 
-  const addFullDay = (title, dayOffset = 0) => {
-    const start = new Date(day.getTime() + dayOffset * 86400000);
-    const end = new Date(start.getTime() + 86400000);
-    addShow(title, start, end);
-  };
-
-  switch (channel) {
-    case 'ppv01':
-    case 'mlb':
-    case 'bsnpr':
-      for (let i = 0; i < 10; i++) {
-        addFullDay('Visite mediaiptv.vercel.app para más detalles', i);
-      }
-      break;
-
-    case 'nba': {
-      const games = [
-        [5, 20, 23, 'NBA Finals presented by Youtube: Indiana Pacers vs Oklahoma City Thunder Game 1'],
-        [8, 20, 23, 'NBA Finals presented by Youtube: Indiana Pacers vs Oklahoma City Thunder Game 2'],
-        [11, 20, 23, 'NBA Finals presented by Youtube: Indiana Pacers vs Oklahoma City Thunder Game 3'],
-        [13, 20, 23, 'NBA Finals presented by Youtube: Indiana Pacers vs Oklahoma City Thunder Game 4'],
-      ];
-      for (let i = 2; i <= 13; i++) {
-        const isGameDay = games.find(g => g[0] === i);
-        if (isGameDay) {
-          addDailyShow('Fuera del Aire', 0, isGameDay[1], i);
-          addDailyShow(isGameDay[3], isGameDay[1], isGameDay[2], i);
-        } else {
-          addFullDay('Fuera del Aire', i);
-        }
-      }
-      break;
-    }
-
-    case 'ewo': {
-      for (let i = 0; i < 14; i++) {
-        const date = new Date(day.getTime() + i * 86400000);
-        const weekday = date.getUTCDay();
-        if (weekday === 6 || weekday === 0) {
-          addFullDay('Fuera del Aire', i);
-        } else {
-          addDailyShow('Fuera del Aire', 0, 8, i);
-          addDailyShow('eWo La Verdad Absoluta', 8, 9.5, i);
-          addDailyShow('Cuenta Regresiva para eWo El Update', 9.5, 10, i);
-          addDailyShow('eWo El Update', 10, 11, i);
-          addDailyShow('eWo ThrowBack', 11, 15, i);
-          addDailyShow('eWo El Perfil', 15, 15.5, i);
-          addDailyShow('eWo 24/7 Retro', 15.5, 17.5, i);
-          addDailyShow('Fuera del Aire', 17.5, 22, i);
-        }
-      }
-      break;
-    }
-  }
-
-  return programs;
-}
-
-export default async function handler(req) {
-  const channels = ['ppv01', 'nba', 'mlb', 'ewo', 'bsnpr'];
-  let xml = '<?xml version="1.0" encoding="UTF-8"?>\n<tv generator-info-name="mediaiptv.vercel.app">\n';
-
-  for (const channel of channels) {
-    xml += `<channel id="${channel}">\n`;
-    xml += `<display-name>${channel.toUpperCase()}</display-name>\n`;
-    xml += `</channel>\n`;
-  }
-
-  for (const channel of channels) {
-    const programs = getProgramsForChannel(channel);
-    for (const p of programs) {
-      xml += `<programme start="${formatDate(p.start)}" stop="${formatDate(p.end)}" channel="${channel}">\n`;
-      xml += `<title>${p.title}</title>\n`;
-      xml += `</programme>\n`;
-    }
-  }
-
-  xml += '</tv>';
+  <!-- BSN PR -->
+  <channel id="bsnpr">
+    <display-name>BSN PR</display-name>
+    <icon src="https://upload.wikimedia.org/wikipedia/en/f/fe/Baloncesto_Superior_Nacional.png" />
+  </channel>
+  <programme start="20250603000000 -0400" stop="20250604000000 -0400" channel="bsnpr">
+    <title>BSN 24/7</title>
+    <desc>Visite mediaiptv.vercel.app para más detalles</desc>
+  </programme>
+</tv>`;
 
   return new Response(xml, {
     headers: {
-      'Content-Type': 'application/xml',
-    },
+      'Content-Type': 'application/xml'
+    }
   });
 }
